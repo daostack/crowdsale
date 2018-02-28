273c6fc8a28e6a762cac49408174fde0d778e5f0
Oren <oren.sokolowsky@gmail.com>
2018-02-28 02:58:56 +0200

    Add partial cap support
    Zepplin 1.7

--- test/daostacksale.js
+++ test/daostacksale.js
@@ -1,14 +1,15 @@
 const helpers = require('./helpers');
-const BigNumber = require('bignumber.js');
+//const BigNumber = require('bignumber.js');
 const DAOstackSale = artifacts.require('./DAOstackSale.sol');
 const MintableToken = artifacts.require('./MintableToken.sol');
+
+const BigNumber = web3.BigNumber;
 
 let token;
 let params;
 let daoStackSale;
 let whiteListed;
 let notWhiteListed;
-
 const setup = async function () {
     let accounts = web3.eth.accounts;
     token = await MintableToken.new();
@@ -18,9 +19,9 @@ const setup = async function () {
     params = {
         owner: accounts[0],
         startDelay: startDelay,
-        startTime: latestTimeStamp + startDelay,
+        openingTime: latestTimeStamp + startDelay,
         duration: duration,
-        endTime: latestTimeStamp + startDelay + duration,
+        closingTime: latestTimeStamp + startDelay + duration,
         rate: 500,
         wallet: accounts[5],
         cap: web3.toWei(20),
@@ -28,8 +29,8 @@ const setup = async function () {
         maxBuy: web3.toWei(10),
     };
     daoStackSale = await DAOstackSale.new(
-        params.startTime,
-        params.endTime,
+        params.openingTime,
+        params.closingTime,
         params.rate,
         params.wallet,
         params.cap,
@@ -37,14 +38,16 @@ const setup = async function () {
         params.maxBuy,
         token.address
     );
-    await daoStackSale.addToWhiteList(accounts[1]);
-    await daoStackSale.addToWhiteList(accounts[2]);
-    await daoStackSale.addToWhiteList(accounts[3]);
+    await daoStackSale.addToWhitelist(accounts[1]);
+    await daoStackSale.addToWhitelist(accounts[2]);
+    await daoStackSale.addToWhitelist(accounts[3]);
     whiteListed = [accounts[1], accounts[2], accounts[3]];
     notWhiteListed = [accounts[4]];
     await token.transferOwnership(daoStackSale.address);
+    //await token.mint(daoStackSale.address,tokenSupply);
     return { daoStackSale, token, params };
 };
+
 
 const buy = async function (buyer, value, shouldSucceed) {
     let preBuyerTokenBalance = await token.balanceOf(buyer);
@@ -54,10 +57,16 @@ const buy = async function (buyer, value, shouldSucceed) {
     // Separate to cases:
     let balanceChange = new BigNumber(0);
     let tokenChange = new BigNumber(0);
+    let capReached = await daoStackSale.capReached();
+    let changeEthBack = new BigNumber(0);
     if (shouldSucceed === true) {
         await web3.eth.sendTransaction( { from: buyer, to: daoStackSale.address, value: value, gas: 200000 } ); // Min sending gas (21k) is not enough
+        if (!capReached && Number(preWeiRaised.plus(value)) > params.cap) {
+            changeEthBack = preWeiRaised.plus(value) - params.cap;
+            value = value - changeEthBack;
+        }
         balanceChange = value;
-        tokenChange = value*params.rate;
+        tokenChange   = value * params.rate;
     } else {
         try {
             await web3.eth.sendTransaction( { from: buyer, to: daoStackSale.address, value: value, gas: 200000 } );  // Min sending gas (21k) is not enough
@@ -71,6 +80,7 @@ const buy = async function (buyer, value, shouldSucceed) {
     assert.equal(postWeiRaised.toString(), (preWeiRaised.plus(balanceChange)).toString(), 'Wrong wei raised variable');
     assert.equal(postWalletBalance.toString(), (preWalletBalance.plus(balanceChange)).toString(), 'Wrong wallet balance');
     assert.equal(postBuyerTokenBalance.toString(), (preBuyerTokenBalance.plus(tokenChange)).toString(), 'Wrong token balance');
+
 };
 
 contract('DAOstackSale', function (accounts)  {
@@ -83,8 +93,8 @@ contract('DAOstackSale', function (accounts)  {
 
         // Check params:
         assert.equal(await daoStackSale.token(), token.address, "Token is not correct");
-        assert.equal(await daoStackSale.startTime(), params.startTime, "startTime is not correct");
-        assert.equal(await daoStackSale.endTime(), params.endTime, "endTime is not correct");
+        assert.equal(await daoStackSale.openingTime(), params.openingTime, "openingTime is not correct");
+        assert.equal(await daoStackSale.closingTime(), params.closingTime, "closingTime is not correct");
         assert.equal(await daoStackSale.wallet(), params.wallet, "Wallet is not correct");
         assert.equal(await daoStackSale.minPay(), params.minBuy, "Min param is not correct");
         assert.equal(await daoStackSale.maxPay(), params.maxBuy, "Max param is not correct");
@@ -123,8 +133,15 @@ contract('DAOstackSale', function (accounts)  {
         await setup();
         await helpers.increaseTime(params.startDelay + 60*60);
         await buy(whiteListed[0], web3.toWei(8), true);
-        await buy(whiteListed[1], web3.toWei(13), false);
+        await buy(whiteListed[1], web3.toWei(8), true);
+        assert.equal(await daoStackSale.capReached(),false);
+        //partial cap
+        await buy(whiteListed[1], web3.toWei(5), true);
+        assert.equal(await daoStackSale.capReached(),true);
+        //cap reached.
+        await buy(whiteListed[1], web3.toWei(1), false);
     });
+
 
     it("Full Scenario 1, cap filled", async () => {
         await setup();
@@ -143,17 +160,27 @@ contract('DAOstackSale', function (accounts)  {
         await buy(whiteListed[1], web3.toWei(11), false);
         await buy(whiteListed[1], web3.toWei(9), true);
 
-        // Trying to go over cap:
-        await buy(whiteListed[2], web3.toWei(8), false);
-
         // Reaching cap:
         await buy(whiteListed[2], web3.toWei(7), true);
+
+        // Try to buy after cap reached:
+        await buy(whiteListed[0], web3.toWei(2), false);
+
+        try {
+            await daoStackSale.finalize();
+        } catch (error) {
+            helpers.assertVMException(error, 'finalize should revert because crowdsale not reached the time cap yet.');
+        }
+
+        await helpers.increaseTime(params.duration);
+
+        // Check finalization:
 
         // Try to buy after finish:
         await buy(whiteListed[0], web3.toWei(2), false);
 
-        // Check finalization:
         await daoStackSale.finalize();
+
         assert.equal(await token.owner(), params.wallet);
 
         // Try to buy again after finish:
@@ -188,13 +215,10 @@ contract('DAOstackSale', function (accounts)  {
         await buy(whiteListed[1], web3.toWei(11), false);
         await buy(whiteListed[1], web3.toWei(9), true);
 
-        // Trying to go over cap:
-        await buy(whiteListed[2], web3.toWei(8), false);
-
         // Reaching time cap:
         await helpers.increaseTime(params.duration);
 
-        // Try to buy after finish:
+        // Try to buy after time cap :
         await buy(whiteListed[0], web3.toWei(2), false);
 
         // Check finalization:
--- test/whitelist.js
+++ test/whitelist.js
@@ -1,42 +1,37 @@
 const helpers = require('./helpers');
-const WhiteList = artifacts.require('./WhiteList.sol');
+const WhiteList = artifacts.require('./WhiteListMock.sol');
 
 const setup = async function () {
-  let whiteList = await WhiteList.new();
-  return whiteList;
+  let whitelist = await WhiteList.new();
+  return whitelist;
 };
 
 contract('WhiteList', function (accounts)  {
 
     it("Check owner", async () => {
-        let whiteList = await setup();
-        let owner = await whiteList.owner();
+        let whitelist = await setup();
+        let owner = await whitelist.owner();
         assert.equal(owner, accounts[0], "Owner was not set correctly");
     });
 
     it("Add to whitelist - owner", async () => {
-        let whiteList = await setup();
-        let tx;
+        let whitelist = await setup();
 
         // Add one:
-        tx = await whiteList.addToWhiteList(accounts[1]);
-        assert(await whiteList.whiteList(accounts[1]), "whitelisting was unseccessfull");
-        assert.equal(await whiteList.whiteListSize(), 1, "whitelist size is not correct");
-        assert.equal(helpers.getValueFromLogs(tx, '_address', 'LogWhiteListAdd'), accounts[1]);
+        await whitelist.addToWhitelist(accounts[1]);
+        assert(await whitelist.whitelist(accounts[1]), "whitelisting was unsuccessful");
 
         // Add second:
-        tx = await whiteList.addToWhiteList(accounts[2]);
-        assert(await whiteList.whiteList(accounts[2]), "whitelisting was unseccessfull");
-        assert.equal(await whiteList.whiteListSize(), 2, "whitelist size is not correct");
-        assert.equal(helpers.getValueFromLogs(tx, '_address', 'LogWhiteListAdd'), accounts[2]);
+        await whitelist.addToWhitelist(accounts[2]);
+        assert(await whitelist.whitelist(accounts[2]), "whitelisting was unsuccessful");
     });
 
     it("Add to whitelist - not owner", async () => {
-        let whiteList = await setup();
+        let whitelist = await setup();
 
         // Try to add one:
         try {
-            await whiteList.addToWhiteList(accounts[0], { from: accounts[1] });
+            await whitelist.addToWhitelist(accounts[0], { from: accounts[1] });
             assert(false, 'Not owenr was able to add to list');
         } catch (error) {
             helpers.assertVMException(error);
@@ -44,75 +39,64 @@ contract('WhiteList', function (accounts)  {
     });
 
     it("Remove from whitelist - owner", async () => {
-        let whiteList = await setup();
-        let tx;
+        let whitelist = await setup();
 
         // Add:
-        await whiteList.addToWhiteList(accounts[1]);
-        await whiteList.addToWhiteList(accounts[2]);
+        await whitelist.addToWhitelist(accounts[1]);
+        await whitelist.addToWhitelist(accounts[2]);
 
         // Remove one:
-        tx = await whiteList.removeFromWhiteList(accounts[1]);
-        assert(!(await whiteList.whiteList(accounts[1])), "removing was unseccessfull");
-        assert(await whiteList.whiteList(accounts[2]), "removing was unseccessfull");
-        assert.equal(await whiteList.whiteListSize(), 1, "whitelist size is not correct");
-        assert.equal(helpers.getValueFromLogs(tx, '_address', 'LogWhiteListRemove'), accounts[1]);
+        await whitelist.removeFromWhitelist(accounts[1]);
+        assert(!(await whitelist.whitelist(accounts[1])), "removing was unsuccessful");
+        assert(await whitelist.whitelist(accounts[2]), "removing was unsuccessful");
 
         // Remove two:
-        tx = await whiteList.removeFromWhiteList(accounts[2]);
-        assert(!(await whiteList.whiteList(accounts[1])), "removing was unseccessfull");
-        assert(!(await whiteList.whiteList(accounts[2])), "removing was unseccessfull");
-        assert.equal(await whiteList.whiteListSize(), 0, "whitelist size is not correct");
-        assert.equal(helpers.getValueFromLogs(tx, '_address', 'LogWhiteListRemove'), accounts[2]);
+        await whitelist.removeFromWhitelist(accounts[2]);
+        assert(!(await whitelist.whitelist(accounts[1])), "removing was unsuccessful");
+        assert(!(await whitelist.whitelist(accounts[2])), "removing was unsuccessful");
     });
 
     it("Remove from whitelist - not owner", async () => {
-        let whiteList = await setup();
+        let whitelist = await setup();
 
         // Add:
-        await whiteList.addToWhiteList(accounts[1]);
+        await whitelist.addToWhitelist(accounts[1]);
 
         // Try to add one:
         try {
-            await whiteList.removeFromWhiteList(accounts[1], { from: accounts[1] });
+            await whitelist.removeFromWhitelist(accounts[1], { from: accounts[1] });
         } catch (error) {
             helpers.assertVMException(error);
         }
-        assert(await whiteList.whiteList(accounts[1]), "non-owner was able to remove");
-        assert(await whiteList.whiteListSize(), 1, "whitelist size is not correct");
+        assert(await whitelist.whitelist(accounts[1]), "non-owner was able to remove");
     });
 
     it("Add twice", async () => {
-        let whiteList = await setup();
+        let whitelist = await setup();
 
         // Add first time:
-        await whiteList.addToWhiteList(accounts[1]);
-        assert(await whiteList.whiteList(accounts[1]), "whitelisting was unseccessfull");
-        assert.equal(await whiteList.whiteListSize(), 1, "whitelist size is not correct");
+        await whitelist.addToWhitelist(accounts[1]);
+        assert(await whitelist.whitelist(accounts[1]), "whitelisting was unsuccessful");
 
         // Add second time:
-        await whiteList.addToWhiteList(accounts[1]);
-        assert(await whiteList.whiteList(accounts[1]), "whitelisting was unseccessfull");
-        assert.equal(await whiteList.whiteListSize(), 1, "whitelist size is not correct");
+        await whitelist.addToWhitelist(accounts[1]);
+        assert(await whitelist.whitelist(accounts[1]), "whitelisting was unsuccessful");
     });
 
     it("Remove twice", async () => {
-        let whiteList = await setup();
+        let whitelist = await setup();
 
         // Add:
-        await whiteList.addToWhiteList(accounts[1]);
-        assert(await whiteList.whiteList(accounts[1]), "whitelisting was unseccessfull");
-        assert.equal(await whiteList.whiteListSize(), 1, "whitelist size is not correct");
+        await whitelist.addToWhitelist(accounts[1]);
+        assert(await whitelist.whitelist(accounts[1]), "whitelisting was unsuccessful");
 
         // Remove first time:
-        await whiteList.removeFromWhiteList(accounts[1]);
-        assert(!(await whiteList.whiteList(accounts[1])), "removing was unseccessfull");
-        assert.equal(await whiteList.whiteListSize(), 0, "whitelist size is not correct");
+        await whitelist.removeFromWhitelist(accounts[1]);
+        assert(!(await whitelist.whitelist(accounts[1])), "removing was unsuccessful");
 
         // Remove second time:
-        await whiteList.removeFromWhiteList(accounts[1]);
-        assert(!(await whiteList.whiteList(accounts[1])), "removing was unseccessfull");
-        assert.equal(await whiteList.whiteListSize(), 0, "whitelist size is not correct");
+        await whitelist.removeFromWhitelist(accounts[1]);
+        assert(!(await whitelist.whitelist(accounts[1])), "removing was unsuccessful");
     });
 
 });
