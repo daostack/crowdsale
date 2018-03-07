5e1906dc679ef176e9faafb089c002a3c9843025
Adam Levi <leviadam@gmail.com>
2018-03-07 10:13:23 +0200

    Turn _setLimits from private to internal

--- contracts/BuyLimitsCrowdsale.sol
+++ contracts/BuyLimitsCrowdsale.sol
@@ -41,7 +41,7 @@ contract BuyLimitsCrowdsale is Crowdsale {
     ** @param _min set the minimum buy in wei
     ** @param _max set the maximum buy in wei, 0 indeicates no maximum
     */
-    function _setLimits(uint _min, uint _max) private {
+    function _setLimits(uint _min, uint _max) internal {
         if (_max != 0) {
             require (_min <= _max); // Sanity Check
         }
