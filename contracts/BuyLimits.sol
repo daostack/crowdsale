ae6626f000d7deac8d4d1aee96c13235e7308fd0
Oren <oren.sokolowsky@gmail.com>
2018-04-10 11:17:21 +0300

    Add daostack presale contract + test

---
+++ contracts/BuyLimits.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.4.21;
+
+contract BuyLimits {
+    event LogLimitsChanged(uint _minBuy, uint _maxBuy);
+
+    // Variables holding the min and max payment in wei
+    uint public minBuy; // min buy in wei
+    uint public maxBuy; // max buy in wei, 0 means no maximum
+
+    /*
+    ** Modifier, reverting if not within limits.
+    */
+    modifier isWithinLimits(uint _amount) {
+        require(withinLimits(_amount));
+        _;
+    }
+
+    /*
+    ** @dev Constructor, define variable:
+    */
+    function BuyLimits(uint _min, uint  _max) public {
+        _setLimits(_min, _max);
+    }
+
+    /*
+    ** @dev Check TXs value is within limits:
+    */
+    function withinLimits(uint _value) public view returns(bool) {
+        if (maxBuy != 0) {
+            return (_value >= minBuy && _value <= maxBuy);
+        }
+        return (_value >= minBuy);
+    }
+
+    /*
+    ** @dev set limits logic:
+    ** @param _min set the minimum buy in wei
+    ** @param _max set the maximum buy in wei, 0 indeicates no maximum
+    */
+    function _setLimits(uint _min, uint _max) internal {
+        if (_max != 0) {
+            require (_min <= _max); // Sanity Check
+        }
+        minBuy = _min;
+        maxBuy = _max;
+        emit LogLimitsChanged(_min, _max);
+    }
+}
---
+++ contracts/DAOstackPreSale.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.4.21;
+
+
+import "./lifecycle/Pausable.sol";
+import "./ownership/Whitelist.sol";
+import "./BuyLimits.sol";
+
+/**
+ * @title DAOstackPresale
+ * @dev A contract to allow only whitelisted followers to participate in presale.
+ */
+contract DAOstackPreSale is Pausable,BuyLimits,Whitelist {
+    event LogFundsReceived(address indexed _sender, uint amount);
+
+    address public wallet; // Amount of wei raised
+
+    /**
+    * @dev Constructor.
+    * @param _wallet Address where the funds are transfered to
+    * @param _minBuy Address where the funds are transfered to
+    * @param _maxBuy Address where the funds are transfered to
+    */
+    function DAOstackPreSale(address _wallet, uint _minBuy, uint _maxBuy)
+    public
+    BuyLimits(_minBuy, _maxBuy)
+    {
+        // Set wallet:
+        require(_wallet != address(0));
+        wallet = _wallet;
+    }
+
+    /**
+    * @dev Fallback, funds coming in are transfered to wallet
+    */
+    function () payable whenNotPaused onlyWhitelisted isWithinLimits(msg.value) external {
+        wallet.transfer(msg.value);
+        emit LogFundsReceived(msg.sender, msg.value);
+    }
+
+    /*
+    ** @dev Drain function, in case of failure. Contract should not hold eth anyhow.
+    */
+    function drain() external {
+        wallet.transfer((address(this)).balance);
+    }
+
+}
